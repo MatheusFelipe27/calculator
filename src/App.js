8eb4d703e56f6aa4ca3e45c7bef62f6e382028c2
matheus <matheusfelipe@opus-software.com.br>
2023-06-03 23:34:12 -0300

    added toggle and sass variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.scss';
 
 function App(){
   const [displayed, setDisplayed] = useState("")
   const [operation, setOperation] = useState("")
   const [prev, setPrev] = useState()
+  const [toggle, setToggle] = useState(false)
+
+
+
+  useEffect(() =>{
+
+  }, [toggle])
 
   const concat = (e) =>{
     var val = e.target.value
@@ -19,13 +26,16 @@ function App(){
     }
     else if(operation==='/'){
       op = parseFloat(prev)/parseFloat(displayed)
-      setDisplayed(op==0? "0": op)    }
+      setDisplayed(op===0? "0": op)    
+    }
     else if(operation==='-'){
       op = parseFloat(prev)-parseFloat(displayed)
-      setDisplayed(op===0? "0": op)    }
+      setDisplayed(op===0? "0": op)    
+    }
     else if(operation==='*'){
       op = parseFloat(prev)*parseFloat(displayed)
-      setDisplayed(op===0? "0": op)    }
+      setDisplayed(op===0? "0": op)    
+    }
     setOperation("")
   }
 
@@ -60,37 +70,49 @@ function App(){
   }
 
   return (
-    <div className='main'>
-      <div className='info'>
-          <h3 className='col'>calc</h3>
+    <>
+      <div className='toggle-div'>
+        <h5 className='theme'>theme</h5>
+      <label className="switch">
+        <input type="checkbox" onClick={()=> setToggle(!toggle)}/>
+        <span className="slider round"></span>
+      </label>
       </div>
-      <div className='ans'>
-        <div className='partial'>
-          <h6 className='partial-val'>{operation? operation : ""}</h6>
+      <div className='outside'>
+        <div className='main'>
+          <div>
+              <h3 className='col'>calc</h3>
+          </div>
+          <div className='ans'>
+            <div className='partial'>
+              <h6 className='partial-val'>{operation? operation : ""}</h6>
+            </div>
+            <h4 className='displayed-val'>{displayed? displayed: ""}</h4>
+          </div>
+          <div className='numbers'>
+              <button className='btn' value='7'onClick={(e)=> concat(e)}> 7</button>
+              <button className='btn' value='8' onClick={ (e)=> concat(e)}> 8</button>
+              <button className='btn' value='9' onClick={(e)=> concat(e)}> 9</button>
+              <button className='del-btn' onClick={del}> del</button>
+              <button className='btn' value='4' onClick={(e)=> concat(e)}> 4</button>
+              <button className='btn' value='5' onClick={(e)=> concat(e)}> 5</button>
+              <button className='btn' value='6' onClick={(e)=> concat(e)}> 6</button>
+              <button className='btn' value='+' onClick={(e)=> {operate(e) }}> +</button>
+              <button className='btn' value='1' onClick={(e)=> concat(e)}> 1</button>
+              <button className='btn' value='2' onClick={(e)=> concat(e)}> 2</button>
+              <button className='btn' value='3' onClick={(e)=> concat(e)}> 3</button>
+              <button className='btn' value='-' onClick={(e)=> operate(e) }> -</button>
+              <button className='btn' value='.' onClick={(e)=> concat(e)}> .</button>
+              <button className='btn' value='0' onClick={(e)=> concat(e)}> 0</button>
+              <button className='btn' value='/' onClick={(e)=> operate(e)}> /</button>
+              <button className='btn' value='*' onClick={(e)=> operate(e)}> x</button>
+              <button className='res-btn' onClick={reset}> reset</button>
+              <button className='equal-btn' onClick={result}> =</button>
+          </div>
         </div>
-          <h4 className='displayed-val'>{displayed? displayed: ""}</h4>
       </div>
-      <div className='numbers'>
-          <button className='btn' value='7'onClick={(e)=> concat(e)}> 7</button>
-          <button className='btn' value='8' onClick={ (e)=> concat(e)}> 8</button>
-          <button className='btn' value='9' onClick={(e)=> concat(e)}> 9</button>
-          <button className='del-btn' onClick={del}> del</button>
-          <button className='btn' value='4' onClick={(e)=> concat(e)}> 4</button>
-          <button className='btn' value='5' onClick={(e)=> concat(e)}> 5</button>
-          <button className='btn' value='6' onClick={(e)=> concat(e)}> 6</button>
-          <button className='btn' value='+' onClick={(e)=> {operate(e) }}> +</button>
-          <button className='btn' value='1' onClick={(e)=> concat(e)}> 1</button>
-          <button className='btn' value='2' onClick={(e)=> concat(e)}> 2</button>
-          <button className='btn' value='3' onClick={(e)=> concat(e)}> 3</button>
-          <button className='btn' value='-' onClick={(e)=> operate(e) }> -</button>
-          <button className='btn' value='.' onClick={(e)=> concat(e)}> .</button>
-          <button className='btn' value='0' onClick={(e)=> concat(e)}> 0</button>
-          <button className='btn' value='/' onClick={(e)=> operate(e)}> /</button>
-          <button className='btn' value='*' onClick={(e)=> operate(e)}> x</button>
-          <button className='res-btn' onClick={reset}> reset</button>
-          <button className='equal-btn' onClick={result}> =</button>
-      </div>
-    </div>
+    </>
+
   );
 }
 
